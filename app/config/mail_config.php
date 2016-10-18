95a526f9e49a7397298d637f15dd4f7c83f02753
Taras Besarab <tbesarab@hs-stp-3>
2016-10-18 14:18:20 +0300

    Create CI project whith Grocery CRUD

---
+++ app/config/mail_config.php
@@ -0,0 +1,9 @@
+<?php
+$config['smtp_username'] = 'voliawebmastervin@yandex.ru';  //Смените на адрес своего почтового ящика.
+$config['smtp_port'] = '465'; // Порт работы.
+$config['smtp_host'] =  'ssl://smtp.yandex.ru';  //сервер для отправки почты
+$config['smtp_password'] = 'voliawebmastervin1234';  //Измените пароль
+$config['smtp_debug'] = true;  //Если Вы хотите видеть сообщения ошибок, укажите true вместо false
+$config['smtp_charset'] = 'utf-8';	//кодировка сообщений. (windows-1251 или utf-8, итд)
+$config['smtp_from'] = 'ADMIN TECH'; //Ваше имя - или имя Вашего сайта. Будет показывать при прочтении в поле "От кого"
+
