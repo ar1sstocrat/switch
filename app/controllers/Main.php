c5021795762f130aa538abe16b2bc918ec2046e1
Taras Besarab <tbesarab@hs-stp-3>
2016-10-18 14:31:38 +0300

    Create Main controller for /

---
+++ app/controllers/Main.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Description of Main
+ *
+ * @author tbesarab
+ */
+class Main extends Public_Controller
+{
+    public function __construct() 
+    {
+        parent::__construct();
+        $this->load->model('mod_main');
+    }
+    
+    public function index()
+    {
+        $this->menu = $this->mod_main->get_menu();
+        $this->firephp->log($this->menu);
+        
+        $this->tpl
+                ->set('current_section', 'Главная')
+                ->set('user', $this->user)
+                ->set('menu', $this->menu)
+                ->set('group_id',  $this->group_id)
+                ->set_view('output','out_data')
+                ->build();
+    }
+}
