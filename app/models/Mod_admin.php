cee3cd058c4bac7d165105ff01b052bf8656d007
Taras Besarab <tbesarab@hs-stp-3>
2016-10-18 15:50:53 +0300

    add model admin

---
+++ app/models/Mod_admin.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Description of Mod_admin
+ *
+ * @author tbesarab
+ */
+class Mod_admin extends MY_Model
+{
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+}
