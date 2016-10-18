d482d174429f6fc797865e02ff4bb5378692bf70
Taras Besarab <tbesarab@hs-stp-3>
2016-10-18 14:26:54 +0300

    Create Main controller for /admin

---
+++ app/controllers/admin/Main.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Description of Main
+ *
+ * @author tbesarab
+ */
+class Main extends Admin_Controller
+{
+    public function index()
+    {
+        $this->tpl
+                ->set('current_section', 'Панель администратора')
+                ->set('menu', $this->menu)
+                ->set('user', $this->user)
+                ->set('group_id',  $this->group_id)
+                ->set_view('output', 'admin/dashboard')
+                ->build('admin/main');
+        $this->firephp->log($this->group_id);
+        $this->firephp->log($this->user);
+        $this->firephp->log($this->session->all_userdata);        
+    }
+}
