5bf51e0f3db8ed46c564d2da404eab1b3caed72b
Taras Besarab <tbesarab@hs-stp-3>
2016-10-19 15:42:09 +0300

    add route

--- app/config/routes.php
+++ app/config/routes.php
@@ -52,4 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-//$route['admin/(:any)/(:any)'] = 'admin/$2';
+$route['admin/login'] = 'admin/auth/login';
+$route['login'] = 'auth/login';
