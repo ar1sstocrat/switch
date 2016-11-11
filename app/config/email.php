d1a7def9a7ac8e9e98c3f67c86392a02e1602e59
Taras Besarab <tbesarab@hs-stp-3>
2016-11-11 14:21:41 +0200

    Include smtp mail config

---
+++ app/config/email.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$config['protocol'] = 'smtp';
+            $config['smtp_host'] = 'smtp.yandex.ru';
+            $config['smtp_user'] = 'voliawebmastervin@yandex.ru';
+            $config['smtp_pass'] = 'voliawebmastervin1234';
+            $config['smtp_port'] = '465';
+            $config['mailtype'] = 'html';
+            $config['smtp_crypto'] = 'ssl';
