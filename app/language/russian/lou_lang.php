0942a5c55b7c6d19c2647a9a57011b4fb0bf22b0
Taras Besarab <tbesarab@hs-stp-3>
2017-01-13 16:13:13 +0200

    Add translate for Lou

---
+++ app/language/russian/lou_lang.php
@@ -0,0 +1,27 @@
+<?php
+/* address base translate*/
+
+$lang['id']                     = '№ п/п';
+$lang['district_id']            = 'Район';
+$lang['street']                 = 'Улица';
+$lang['house']                  = 'Дом';
+$lang['house_id']               = 'Идентификатор дома';
+$lang['locality_id']            = 'Участок';
+$lang['lou_id']                 = 'ЛОУ';
+$lang['net_type']               = 'Тип сети';
+$lang['house_type']             = 'Тип дома';
+$lang['cascades']               = 'Каскад';
+$lang['numders_department']     = 'Количество квартир';
+$lang['comment']                = 'Комментарий';
+
+
+/*lou translate*/
+
+$lang['number']                 = 'Номер';
+$lang['address']                = 'Адрес';
+$lang['pgs_id']                 = 'ПГС';
+$lang['coating']                = 'Покрытие';
+$lang['year']                   = 'Год установки';
+$lang['mounth']                 = 'Месяц установки';
+$lang['name']                   = 'Название';
+
